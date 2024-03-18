daebddbd9ed58cbae50e060cc6528d61f6910703
zzzddyy <44665318+hhhddyy@users.noreply.github.com>
2024-03-18 19:21:53 +0800

    fix bug of cvxpy

--- Fed/Server.py
+++ Fed/Server.py
@@ -1,4 +1,70 @@
+import cvxpy as cp
+import numpy as np
+
+
+
+def cosine_similarity(x, y):
+    # print(x,y,x @ y)
+    # print(cp.sum(x * y),np.dot(x,y) )
+    # exit()
+    return np.dot(x,y)/ (np.linalg.norm(x) * np.linalg.norm(y))
+    # return x @ y / (cp.norm(x) * cp.norm(y))
+
+
+class Server:
+    def __init__(self, client_gradients):
+        self.client_gradients = client_gradients  # List of gradients from clients
+        self.C = len(client_gradients)
+        self.phi = cp.Variable(self.client_gradients[0].shape[0])
+        print(self.client_gradients[0].shape[0])
+
+    def update_vector(self):
+        C = self.C
+        grad_matrix = np.stack(self.client_gradients)
+
+
+
+        # Compute the cosine similarity matrix for the gradients
+        A_upper_left = np.array([[cosine_similarity(grad_matrix[i], grad_matrix[j]) for j in range(C)] for i in range(C)])
+        A_upper_right = cp.hstack([self.phi @ grad_matrix[i] for i in range(C)])
+        A_upper_right=A_upper_right.reshape((-1,1))
+        A_lower_left = A_upper_right.T
+
+        # Construct the full matrix A
+        A_lower_right = np.array([[1]])
+        A_top = cp.hstack([A_upper_left, A_upper_right])
+        A_bottom = cp.hstack([A_lower_left, A_lower_right])
+        A = cp.vstack([A_top, A_bottom])
+
+        B = np.full((C + 1, C + 1), 1 / (C ** 2))
+        B[C,C] = 1
+        for i in range(C):
+            B[i, C] = -2 / C
+            B[C,i] = -2/ C
+
+        # Define the optimization problem to maximize tr(AB)
+        objective = cp.Maximize(cp.trace(A @ B))
+        constraints = [cp.sum_squares(self.phi) <= self.client_gradients[0].shape[0]**2]
+
+
+        prob = cp.Problem(objective,constraints)
+        prob.solve(solver=cp.SCS)
+
+        # Update the global vector (phi) based on the solution
+        #self.phi.value = self.phi.value / np.linalg.norm(self.phi.value)  # Normalize phi
+        print(self.phi.value,prob.value)
+        return self.phi
+
+
+# # Example usage
+# client_gradients = [np.random.rand(10) for _ in range(5)]  # Example client gradients
+# for i in range(5):
+#     client_gradients[i] /= np.linalg.norm(client_gradients[i])
 #
-# class CFCServer():
-#     #todo pass back the cud and cum
+#
+# server = Server(client_gradients)
+# global_vector = server.update_vector()
+# print(global_vector.value)
+
+
 
--- main.py
+++ main.py
@@ -1,16 +1,83 @@
-# This is a sample Python script.
+import sys
+sys.path.append("../")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import warnings
+warnings.filterwarnings("ignore")
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+from dataPre import data_set,data_dict
+import numpy as np
+class dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+args = dotdict()
+
+# ================ change the path to dataset !
+args.root_path =  r"C:\Users\hhhddyy\Desktop\NTU-Tan\phd\FedCFC\data\UCIHAR"
+# ================ If using spectrogram as input, modify the path where spectrogram is stored
+args.freq_save_path = None
+# ================
+args.data_name = "ucihar"
+
+args.difference = True
+args.sampling_freq =   50
+args.train_vali_quote = 0.95
+# for this dataset the window size is 128
+args.windowsize = int(2.56 * args.sampling_freq)
+
+args.drop_long = False
+
+args.datanorm_type = "standardization" # None ,"standardization", "minmax"
+
+args.wavename = "morl"
+# if you want to use raw time series as input, set model_type as time
+# if you want to use spectrogram as input, set model_type as freq
+# if you want to use both as inputs, set model_type as cross
+args.model_type = "time"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# if you want to do the given train test experiment, set exp_mode as "Given"
+# if you want to do Semi non overlapping experiment, set exp_mode as "SOCV"
+# if you want to do Full non overlapping experiment, set exp_mode as "FOCV"
+args.exp_mode = "Given"
+
+if args.exp_mode == "FOCV":
+    args.displacement =  int(1 * args.windowsize)
+else:
+    args.displacement =  int(0.5 * args.windowsize)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+args.batch_size = 32
+args.shuffle = True
+args.drop_last = False
+dataset = data_dict[args.data_name](args)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("================ {} Mode ====================".format(dataset.exp_mode))
+print("================ {} CV ======================".format(dataset.num_of_cv))
+for i in range(dataset.num_of_cv):
+    dataset.update_train_val_test_keys()
+    train_data = data_set(args, dataset, "train")
+    test_data = data_set(args, dataset, "test")
+    vali_data = data_set(args, dataset, "vali")
+
+    # form the dataloader
+    train_data_loader = DataLoader(train_data,
+                                   batch_size=args.batch_size,
+                                   shuffle=args.shuffle,
+                                   num_workers=0,
+                                   drop_last=args.drop_last)
+
+    vali_data_loader = DataLoader(vali_data,
+                                  batch_size=args.batch_size,
+                                  shuffle=args.shuffle,
+                                  num_workers=0,
+                                  drop_last=args.drop_last)
+
+    test_data_loader = DataLoader(test_data,
+                                  batch_size=args.batch_size,
+                                  shuffle=args.shuffle,
+                                  num_workers=0,
+                                  drop_last=args.drop_last)
